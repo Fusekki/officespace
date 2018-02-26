84fcde7890502caa6d9d634d08fcadbe4df3df94
Phillip Stafford <phillip@philliprstafford.com>
2018-02-26 16:40:52 -0500

    feat:
    Add case-item component.  This item will store the properties of the case item.

---
+++ src/app/components/case-item/case-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-case-item',
+  templateUrl: './case-item.component.html',
+  styleUrls: ['./case-item.component.css']
+})
+export class CaseItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
