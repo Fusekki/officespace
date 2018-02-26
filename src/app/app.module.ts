db60fd35f2d0de6f727cd984292f82d39cdc1861
Phillip Stafford <phillip@philliprstafford.com>
2018-02-26 16:41:02 -0500

    feat:
    Add case-item component.  This item will store the properties of the case item.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import { CaseItemComponent } from './components/case-item/case-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CaseItemComponent
   ],
   imports: [
     BrowserModule,
