20ff56c17dda2ea030e770594435c140590a08b8
Phillip Stafford <phillip@philliprstafford.com>
2018-02-27 18:32:36 -0500

    # If applied, this commit will... Add the MessageService and LegalcaseService to the app.
    # Why is this change needed?
    The services need to be imported to be available and set as Providers.
    
    # How does it address the issue?
    It will make them available to the app.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+
 import { CaseItemComponent } from './components/case-item/case-item.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { CasesComponent } from './components/cases/cases.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MessageService } from './services/message.service';
+import { LegalcaseService } from './services/legalcase.service';
 
 @NgModule({
   declarations: [
@@ -23,6 +28,7 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
@@ -36,7 +42,10 @@ import { HttpClientModule } from '@angular/common/http';
     MatIconModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    LegalcaseService,
+    MessageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
