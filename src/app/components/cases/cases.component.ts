014b08638e84d3e83a95342cd49036ffcd54531a
Phillip Stafford <phillip@philliprstafford.com>
2018-02-27 13:55:47 -0500

    # If applied, this commit will... Add the cases component to the app.
    # Why is this change needed?
    This component will store the methods to manipulate the cases array.
    
    # How does it address the issue?
    This addition creates the logic behind the maniplulation of the array.
    
    # Provide links to any relevant tickets, articles or other resources

---
+++ src/app/components/cases/cases.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Case } from '../../classes/case';
+import { CaseService} from '../../services/case.service';
+
+@Component({
+  selector: 'app-cases',
+  templateUrl: './cases.component.html',
+  styleUrls: ['./cases.component.css']
+})
+export class CasesComponent implements OnInit {
+
+  constructor(private caseService: CaseService) { }
+
+  ngOnInit() {
+    this.getCases();
+  }
+
+  getCases(): void {
+    this.caseService.getCases()
+      .subscribe(cases => this.cases = cases);
+  }
+
+  add(title: string): void {
+    title = name.trim();
+    if (!name) { return; }
+    this.caseService.addCase({ title } as Case)
+      .subscribe(case => {
+        this.cases.push(case);
+      });
+  }
+
+  delete(case: Case): void {
+    this.cases = this.cases.filter(c => c !== case);
+    this.caseService.deleteCase(case).subscribe();
+  }
+
+}
