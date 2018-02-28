2d25837a21b5bbdd983431719392033887455a7b
Phillip Stafford <phillip@philliprstafford.com>
2018-02-28 12:15:44 -0500

    # If applied, this commit will... Remove legalcase-item and replace with legalcase-detail.
    # Why is this change needed?
    We removed the old component and am replacing with a new one.
    
    # How does it address the issue?
    It now imports the newly named component and doesn't import the deleted one. The route has also been adjusted for the new component name.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LegalCasesComponent } from './components/legalcases/legalcases.component';
-import { LegalCaseItemComponent } from './components/legalcase-item/legalcase-item.component';
+import { LegalcaseDetailComponent } from './components/legalcase-detail/legalcase-detail.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: LegalCaseItemComponent },
+  { path: 'detail/:id', component: LegalcaseDetailComponent },
   { path: 'legalcases', component: LegalCasesComponent }
 ];
 
