f6aa1b14d07ea276b86ef18676d8afcf7062fac8
Phillip Stafford <phillip@philliprstafford.com>
2018-02-27 20:13:56 -0500

    # If applied, this commit will... Add imports for ActivatedRoute, Location, Legalcase and LegalcaseService. Add getLegalcase, goBack, and save methods and logic to them.
    # Why is this change needed?
    This will make all of these modules available to the component.
    Makes the methods available.
    
    # How does it address the issue?
    It imports them.
    It creates the methods and defines them.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/components/legalcase-item/legalcase-item.component.ts
+++ src/app/components/legalcase-item/legalcase-item.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Legalcase } from '../../classes/legalcase';
+import { LegalcaseService } from '../../services/legalcase.service';
 
 @Component({
   selector: 'app-legalcase-item',
@@ -6,10 +11,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./legalcase-item.component.css']
 })
 export class LegalCaseItemComponent implements OnInit {
+  @Input() legalCase: Legalcase;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private legalcaseService: LegalcaseService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getLegalcase();
+  }
+
+  getLegalcase(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.legalcaseService.getLegalcase(id)
+      .subscribe(legalCase => this.legalCase = legalCase);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.legalcaseService.updateLegalcase(this.legalCase)
+      .subscribe(() => this.goBack());
   }
 
 }
