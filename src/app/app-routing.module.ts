8a5e431b7b0a352ba39983ed0a95758a2b281098
Phillip Stafford <phillip@philliprstafford.com>
2018-02-27 20:17:15 -0500

    # If applied, this commit will... Add route for detail/:id and sets the LegalCasesComponent to it. Add import of the LegalCasesComponent.
    # Why is this change needed?
    So that the route works in the html.
    
    # How does it address the issue?
    It creates the route.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { LegalCasesComponent } from './components/legalcases/legalcases.component';
 import { LegalCaseItemComponent } from './components/legalcase-item/legalcase-item.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'legalcases', component: LegalCaseItemComponent }
+  { path: 'detail/:id', component: LegalCaseItemComponent },
+  { path: 'legalcases', component: LegalCasesComponent }
 ];
 
 
