797ca0a8681727602284d16846d978713845dcea
Phillip Stafford <phillip@philliprstafford.com>
2018-02-28 11:46:52 -0500

    # If applied, this commit will... Add inital code for component.
    # Why is this change needed?
    So that the dashboard component will get all the legalcases.
    
    # How does it address the issue?
    It creates the component.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Legalcase } from '../../classes/legalcase';
+import { LegalcaseService } from '../../services/legalcase.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  legalcases: Legalcase[] = [];
+
+  constructor(private legalcaseService: LegalcaseService) { }
 
   ngOnInit() {
+    this.getLegalcases();
+  }
+
+  getLegalcases(): void {
+    this.legalcaseService.getLegalcases()
+      .subscribe(legalcases => this.legalcases = legalcases.slice(1, 5));
   }
 
 }
