4e37125bde9dd8436f0cc1fcbd627df78e37917e
Phillip Stafford <phillip@philliprstafford.com>
2018-02-28 10:16:31 -0500

    # If applied, this commit will... Add mock legalcase objects to app.
    # Why is this change needed?
    To have mock objects so that we can test the legalcase-item.
    
    # How does it address the issue?
    It creates the mock objects.
    
    # Provide links to any relevant tickets, articles or other resources

---
+++ src/app/mock-legalcases.ts
@@ -0,0 +1,6 @@
+import { Legalcase } from './classes/legalcase';
+
+export const LEGALCASES: Legalcase[] = [
+  { id: 11, title: 'People vs. OJ Simpson' },
+  { id: 12, title: 'People vs. DJ Trump' }
+];
