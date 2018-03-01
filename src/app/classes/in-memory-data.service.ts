4a6edbc036667fc80f205956ee4f999e25fb5be8
Phillip Stafford <phillip@philliprstafford.com>
2018-03-01 12:56:26 -0500

    # If applied, this commit will... Add docket and defendant properties to the legalcase object.
    # Why is this change needed?
    The dashboard should display this additional info.
    
    # How does it address the issue?
    It adds the properties to the object.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/classes/in-memory-data.service.ts
+++ src/app/classes/in-memory-data.service.ts
@@ -3,9 +3,9 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const legalcases = [
-      { id: 0, title: 'People vs. Someone', cols: 2, rows: 1, color: 'lightblue' },
-      { id: 1, title: 'People vs. OJ Simpson', cols: 2, rows: 2, color: 'lightgreen' },
-      { id: 2, title: 'People vs. DJ Trump', cols: 2, rows: 1, color: 'lightpink' }
+      { id: 0, title: 'People vs. Someone', docket:'12345', defendant:'Someone', cols: 2, rows: 1, color: 'lightblue' },
+      { id: 1, title: 'People vs. OJ Simpson', docket:'23456', defendant:'O.J. Simpson', cols: 2, rows: 2, color: 'lightgreen' },
+      { id: 2, title: 'People vs. Paul Manafort', docket:'34567', defendant:'Manafort, Paul', cols: 2, rows: 1, color: 'lightpink' }
     ];
     return {legalcases};
   }
