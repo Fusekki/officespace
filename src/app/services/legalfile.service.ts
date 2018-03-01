705d61e1a20b01d634fbfc90b9727fad6a9d5a5b
Phillip Stafford <phillip@philliprstafford.com>
2018-03-01 18:26:03 -0500

    # If applied, this commit will... Copy over legalcase.service code and replace 'case' with 'file' to get code working.
    # Why is this change needed?
    So we have the service for the legalfiles.
    We can use the same code from legalcase since they are basically doing the same methods atm.
    
    # How does it address the issue?
    It defines the service.
    
    # Provide links to any relevant tickets, articles or other resources

---
+++ src/app/services/legalfile.service.ts
@@ -0,0 +1,121 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Legalfile } from '../classes/legalfile';
+import { MessageService } from './message.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class LegalfileService {
+
+  private legalfilesUrl = 'api/legalfiles';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** GET legalfilees from the server */
+  getLegalfiles (): Observable<Legalfile[]> {
+    return this.http.get<Legalfile[]>(this.legalfilesUrl)
+      .pipe(
+        tap(legalfiles => this.log(`fetched legal files`)),
+        catchError(this.handleError('getLegalfiles', []))
+      );
+  }
+
+  /** GET legalfile by id. Return `undefined` when id not found */
+  getLegalfileNo404<Data>(id: number): Observable<Legalfile> {
+    const url = `${this.legalfilesUrl}/?id=${id}`;
+    return this.http.get<Legalfile[]>(url)
+      .pipe(
+        map(legalfiles => legalfiles[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? `fetched` : `did not find`;
+          this.log(`${outcome} legalfile id=${id}`);
+        }),
+        catchError(this.handleError<Legalfile>(`getLegalfile id=${id}`))
+      );
+  }
+
+  /** GET legalfile by id. Will 404 if id not found */
+  getLegalfile(id: number): Observable<Legalfile> {
+    const url = `${this.legalfilesUrl}/${id}`;
+    return this.http.get<Legalfile>(url).pipe(
+      tap(_ => this.log(`fetched legalfile id=${id}`)),
+      catchError(this.handleError<Legalfile>(`getLegalfile id=${id}`))
+    );
+  }
+
+  /* GET legalfilees whose name contains search term */
+  searchLegalfiles(term: string): Observable<Legalfile[]> {
+    if (!term.trim()) {
+      // if not search term, return empty legalfile array.
+      return of([]);
+    }
+    return this.http.get<Legalfile[]>(`api/legalfiles/?title=${term}`).pipe(
+      tap(_ => this.log(`found legalfiles matching "${term}"`)),
+      catchError(this.handleError<Legalfile[]>('searchLegalfiles', []))
+    );
+  }
+
+  //////// Save methods //////////
+
+  /** POST: add a new legalfile to the server */
+  addLegalfile (legalfile: Legalfile): Observable<Legalfile> {
+    return this.http.post<Legalfile>(this.legalfilesUrl, legalfile, httpOptions).pipe(
+      tap((legalfile: Legalfile) => this.log(`added legalfile w/ id=${legalfile.id}`)),
+      catchError(this.handleError<Legalfile>('addLegalfile'))
+    );
+  }
+
+  /** DELETE: delete the legalfile from the server */
+  deleteLegalfile (legalfile: Legalfile | number): Observable<Legalfile> {
+    const id = typeof legalfile === 'number' ? legalfile : legalfile.id;
+    const url = `${this.legalfilesUrl}/${id}`;
+
+    return this.http.delete<Legalfile>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted legalfile id=${id}`)),
+      catchError(this.handleError<Legalfile>('deleteLegalfile'))
+    );
+  }
+
+  /** PUT: update the legalfile on the server */
+  updateLegalfile (legalfile: Legalfile): Observable<any> {
+    return this.http.put(this.legalfilesUrl, legalfile, httpOptions).pipe(
+      tap(_ => this.log(`updated legalfile id=${legalfile.id}`)),
+      catchError(this.handleError<any>('updateLegalfile'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a LegalfileService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('LegalfileService: ' + message);
+  }
+}
