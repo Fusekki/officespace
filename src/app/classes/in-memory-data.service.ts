d82bb57562b2fcc304bc5b3d5ba23bc501852bf4
Phillip Stafford <phillip@philliprstafford.com>
2018-03-01 11:50:08 -0500

    Change the legalcases object to store a cols, rows and color value.  These will pass into the style.
    I am going to test out feeding the style from the model.
    
    Stores the values within the mock model.
    
    See previous commit for html and CSS changes.

--- src/app/classes/in-memory-data.service.ts
+++ src/app/classes/in-memory-data.service.ts
@@ -3,9 +3,9 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const legalcases = [
-      { id: 0, title: 'People vs. Someone' },
-      { id: 1, title: 'People vs. OJ Simpson' },
-      { id: 2, title: 'People vs. DJ Trump' }
+      { id: 0, title: 'People vs. Someone', cols: 3, rows: 1, color: 'lightblue' },
+      { id: 1, title: 'People vs. OJ Simpson', cols: 1, rows: 2, color: 'lightgreen' },
+      { id: 2, title: 'People vs. DJ Trump', cols: 1, rows: 1, color: 'lightpink' }
     ];
     return {legalcases};
   }
