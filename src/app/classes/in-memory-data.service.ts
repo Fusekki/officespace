52396eeed101347262392fdd4e64d64dd625fdcb
Phillip Stafford <phillip@philliprstafford.com>
2018-03-01 12:42:27 -0500

    # If applied, this commit will... Change the rows and column values of some objects.
    # Why is this change needed?
    Styling.
    
    # How does it address the issue?
    Sets new values for each attribute.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/classes/in-memory-data.service.ts
+++ src/app/classes/in-memory-data.service.ts
@@ -3,9 +3,9 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const legalcases = [
-      { id: 0, title: 'People vs. Someone', cols: 3, rows: 1, color: 'lightblue' },
-      { id: 1, title: 'People vs. OJ Simpson', cols: 1, rows: 2, color: 'lightgreen' },
-      { id: 2, title: 'People vs. DJ Trump', cols: 1, rows: 1, color: 'lightpink' }
+      { id: 0, title: 'People vs. Someone', cols: 2, rows: 1, color: 'lightblue' },
+      { id: 1, title: 'People vs. OJ Simpson', cols: 2, rows: 2, color: 'lightgreen' },
+      { id: 2, title: 'People vs. DJ Trump', cols: 2, rows: 1, color: 'lightpink' }
     ];
     return {legalcases};
   }
