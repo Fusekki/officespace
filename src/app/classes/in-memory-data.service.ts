c136538a2566344a24573d2ed4ccb8b5e6e9903e
Phillip Stafford <phillip@philliprstafford.com>
2018-03-02 10:53:25 -0500

    # If applied, this commit will... Add mock company.
    # Why is this change needed?
    So that we can utilize the companies model info.
    
    # How does it address the issue?
    It creates the mock entry.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/classes/in-memory-data.service.ts
+++ src/app/classes/in-memory-data.service.ts
@@ -12,6 +12,9 @@ export class InMemoryDataService implements InMemoryDbService {
       { id: 1, legalcase_id: 0, title: 'Document2', icon: 'word' },
       { id: 2, legalcase_id: 0, title: 'Spreadsheet1', icon: 'excel' }
     ];
-    return {legalcases, legalfiles};
+    const companies = [
+      { id: 0,  title: 'Law Firm' }
+    ];
+    return {legalcases, legalfiles, companies};
   }
 }
