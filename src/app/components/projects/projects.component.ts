eb02fce5a7c85a50aed31d72c9fb92982a0c0f7c
Phillip Stafford <phillip@philliprstafford.com>
2018-03-02 13:27:49 -0500

    # If applied, this commit will... Import the team class. Create mock company, project, user and team entries. Create team arrays. Push mock items to arrays.
    # Why is this change needed?
    We need the mock items for views.
    
    # How does it address the issue?
    It creates the mock items.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Project } from '../../classes/project';
 import { User } from '../../classes/user';
+import { Team } from '../../classes/team';
 import { Company } from '../../classes/company';
 import { ProjectsService } from '../../services/projects.service';
 
@@ -12,15 +13,21 @@ import { ProjectsService } from '../../services/projects.service';
 export class ProjectsComponent implements OnInit {
 
   projects: Project[] = [];
+  teams: Team[] = [];
 
   constructor(private projectsService: ProjectsService) { }
 
-  private company = new Company(0, "Law Firm");
-  private user = new User(0, "ying@staffordesq.com", 'Ying Stafford');
+  private company = new Company(0, 'Law Firm');
+  private user = new User(0, 'ying@staffordesq.com', 'Ying Stafford');
+  private project = new Project(0, 'Some project');
+  private team = new Team(0, 'Some team');
+
+  projects.push(project);
+  teams.push(team);
+
 
   ngOnInit() {
     this.getProjects();
-    this.company.id = 0;
   }
 
   getProjects(): void {
