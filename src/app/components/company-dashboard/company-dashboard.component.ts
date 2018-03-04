ecc3652a1569af1959d1fc728e8fa2e8217c77cd
Phillip Stafford <phillip@philliprstafford.com>
2018-03-04 17:21:46 -0500

    # If applied, this commit will... Create the company-dashboard component.
    # Why is this change needed?
    We are creating a route for companies/id and we need a component to tie it to.
    
    # How does it address the issue?
    It creates the component along with starter HTML.
    
    # Provide links to any relevant tickets, articles or other resources

---
+++ src/app/components/company-dashboard/company-dashboard.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Company } from '../../classes/company';
+import { CompanyService } from '../../services/company.service';
+
+
+@Component({
+  selector: 'app-company-dashboard',
+  templateUrl: './company-dashboard.component.html',
+  styleUrls: ['./company-dashboard.component.css']
+})
+export class CompanyDashboardComponent implements OnInit {
+
+  companies: Company[] = [];
+
+  constructor(private companyService: CompanyService) { }
+
+  ngOnInit() {
+    this.getCompanies();
+  }
+
+  getCompanies(): void {
+    this.companyService.getCompanies()
+      .subscribe(companies => this.companies = companies);
+  }
+
+}
