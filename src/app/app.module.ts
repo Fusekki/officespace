e1fbba93b507487e82b82e49046b36b836797e4e
Phillip Stafford <phillip@philliprstafford.com>
2018-03-03 19:40:48 -0500

    # If applied, this commit will... Added import of ReactiveFormsModule and MatSelectModule.
    # Why is this change needed?
    We are using these modules in the team-detail view.
    
    # How does it address the issue?
    It imports the modules so the component can use them.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
@@ -10,6 +10,7 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSelectModule} from '@angular/material/select';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
@@ -44,12 +45,14 @@ import { TeamDetailComponent } from './components/team-detail/team-detail.compon
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     MatMenuModule,
     MatGridListModule,
     MatButtonModule,
     MatIconModule,
     MatCardModule,
     MatTooltipModule,
+    MatSelectModule,
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
@@ -60,13 +63,16 @@ import { TeamDetailComponent } from './components/team-detail/team-detail.compon
   ],
   exports: [
     BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     MatButtonModule,
     MatCardModule,
     MatGridListModule,
     MatIconModule,
     MatMenuModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatSelectModule
   ],
   providers: [
     LegalcaseService,
