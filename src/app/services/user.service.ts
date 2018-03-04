ece4430599c61ef9cd6db33a966f116808137813
Phillip Stafford <phillip@philliprstafford.com>
2018-03-04 17:23:06 -0500

    # If applied, this commit will... Create the user service
    # Why is this change needed?
    We are thinking of using it for project-component.
    
    # How does it address the issue?
    It creates the service thus making it available.
    
    # Provide links to any relevant tickets, articles or other resources

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,122 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { User } from '../classes/user';
+import { MessageService } from './message.service';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable()
+export class UserService {
+
+  private usersUrl = 'api/users';  // URL to web api
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  /** GET Useres from the server */
+  getUsers (): Observable<User[]> {
+    return this.http.get<User[]>(this.usersUrl)
+      .pipe(
+        tap(users => this.log(`fetched legal files`)),
+        catchError(this.handleError('getusers', []))
+      );
+  }
+
+  /** GET User by id. Return `undefined` when id not found */
+  getUserNo404<Data>(id: number): Observable<User> {
+    const url = `${this.usersUrl}/?id=${id}`;
+    return this.http.get<User[]>(url)
+      .pipe(
+        map(users => users[0]), // returns a {0|1} element array
+        tap(h => {
+          const outcome = h ? `fetched` : `did not find`;
+          this.log(`${outcome} User id=${id}`);
+        }),
+        catchError(this.handleError<User>(`getUser id=${id}`))
+      );
+  }
+
+  /** GET User by id. Will 404 if id not found */
+  getUser(id: number): Observable<User> {
+    const url = `${this.usersUrl}/${id}`;
+    return this.http.get<User>(url).pipe(
+      tap(_ => this.log(`fetched User id=${id}`)),
+      catchError(this.handleError<User>(`getUser id=${id}`))
+    );
+  }
+
+  /* GET Useres whose name contains search term */
+  searchusers(term: string): Observable<User[]> {
+    if (!term.trim()) {
+      // if not search term, return empty User array.
+      return of([]);
+    }
+    return this.http.get<User[]>(`api/users/?title=${term}`).pipe(
+      tap(_ => this.log(`found users matching "${term}"`)),
+      catchError(this.handleError<User[]>('searchusers', []))
+    );
+  }
+
+  //////// Save methods //////////
+
+  /** POST: add a new User to the server */
+  addUser (User: User): Observable<User> {
+    return this.http.post<User>(this.usersUrl, User, httpOptions).pipe(
+      tap((User: User) => this.log(`added User w/ id=${User.id}`)),
+      catchError(this.handleError<User>('addUser'))
+    );
+  }
+
+  /** DELETE: delete the User from the server */
+  deleteUser (User: User | number): Observable<User> {
+    const id = typeof User === 'number' ? User : User.id;
+    const url = `${this.usersUrl}/${id}`;
+
+    return this.http.delete<User>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted User id=${id}`)),
+      catchError(this.handleError<User>('deleteUser'))
+    );
+  }
+
+  /** PUT: update the User on the server */
+  updateUser (User: User): Observable<any> {
+    return this.http.put(this.usersUrl, User, httpOptions).pipe(
+      tap(_ => this.log(`updated User id=${User.id}`)),
+      catchError(this.handleError<any>('updateUser'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a userservice message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('userservice: ' + message);
+  }
+
+}
