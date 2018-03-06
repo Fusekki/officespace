303e11184b6802e739c01710a58a33d86a25838b
Phillip Stafford <phillip@philliprstafford.com>
2018-03-06 12:28:01 -0500

    # If applied, this commit will... Add logic for watercooler component.  Add necessary modules for import including the class and others.
    # Why is this change needed?
    We need the base logic to make requests to the service and communicate that data.
    
    # How does it address the issue?
    It creates the starting logic.
    
    # Provide links to any relevant tickets, articles or other resources

--- src/app/components/watercooler/watercooler.component.ts
+++ src/app/components/watercooler/watercooler.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { Watercooler } from '../../classes/watercooler';
+import { WatercoolerService } from '../../services/watercooler.service';
+
 @Component({
   selector: 'app-watercooler',
   templateUrl: './watercooler.component.html',
@@ -7,9 +12,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WatercoolerComponent implements OnInit {
 
-  constructor() { }
+  watercooler: Watercooler;
+
+  constructor(private watercoolerService: WatercoolerService,
+              private route: ActivatedRoute,
+              private location: Location) { }
 
   ngOnInit() {
+    this.getWatercooler();
+  }
+
+  getWatercooler(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.watercoolerService.getWatercooler(id)
+      .subscribe(watercooler => this.watercooler = watercooler);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.watercoolerService.updateWatercooler(this.watercooler)
+      .subscribe(() => this.goBack());
   }
 
 }
