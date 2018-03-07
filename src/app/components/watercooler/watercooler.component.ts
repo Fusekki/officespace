b9c0dfe493b9be3439e0e7a92c8d2b2bf8d33762
Phillip Stafford <phillip@philliprstafford.com>
2018-03-06 19:38:37 -0500

    # If applied, this commit will... Import Company and Company service. Add subscribe call after .map service call, passing the value.
    # Why is this change needed?
    Have figured out how to chain the subscribe after a map.
    
    # How does it address the issue?
    Allows us to make concurrent calls to two services and pass the result of the first to the second.
    
    # Provide links to any relevant tickets, articles or other resources
    Previous commit. Next commit.

--- src/app/components/watercooler/watercooler.component.ts
+++ src/app/components/watercooler/watercooler.component.ts
@@ -3,8 +3,14 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { Company } from '../../classes/company';
 import { Watercooler } from '../../classes/watercooler';
 import { WatercoolerService } from '../../services/watercooler.service';
+import { CompanyService } from '../../services/company.service';
+
 
 @Component({
   selector: 'app-watercooler',
@@ -14,19 +20,34 @@ import { WatercoolerService } from '../../services/watercooler.service';
 export class WatercoolerComponent implements OnInit {
 
   watercooler: Watercooler;
+  company: Company;
 
   constructor(private watercoolerService: WatercoolerService,
-              private route: ActivatedRoute,
-              private location: Location) { }
+    private companyService: CompanyService,
+    private route: ActivatedRoute,
+    private location: Location) { }
 
   ngOnInit() {
-    this.getWatercooler();
+    // this.getWatercooler();
+    this.getWatercooler().subscribe(_ => {
+      ;
+      console.log(this.watercooler.company_id);
+      var id = this.watercooler.company_id;
+      this.companyService.getCompany(id)
+        .subscribe(company => this.company = company);
+    });
   }
 
   getWatercooler(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    this.watercoolerService.getWatercooler(id)
-      .subscribe(watercooler => this.watercooler = watercooler);
+    return this.watercoolerService.getWatercooler(id)
+      .map(watercooler => this.watercooler = watercooler);
+  }
+
+  getCompany(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.companyService.getCompany(id)
+      .subscribe(company => this.company = company);
   }
 
   goBack(): void {
