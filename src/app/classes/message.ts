1076be52348e42f4c886f7b97598ce6d68efb724
Phillip Stafford <phillip@philliprstafford.com>
2018-03-07 18:09:36 -0500

    Remove time property.

--- src/app/classes/message.ts
+++ src/app/classes/message.ts
@@ -1,17 +1,15 @@
 export class Message {
   id: number;
   author: string;
-  date: string;
-  time: string;
+  created: string;
   content: string;
   watercooler_id: number;
 
 
-  constructor(id: number, author: string, date: string, time: string, text: string, watercooler_id: number) {
+  constructor(id: number, author: string, created: string, text: string, watercooler_id: number) {
     this.id = id;
     this.author = author;
-    this.date = date;
-    this.time = time;
+    this.created = created;
     this.content = text;
     this.watercooler_id = watercooler_id
   }
