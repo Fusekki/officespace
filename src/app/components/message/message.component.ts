4538bfb494310b4ace75547821f8f232dd80aa82
Phillip Stafford <phillip@philliprstafford.com>
2018-03-07 18:10:47 -0500

    Feat: Add DataPipe module.

--- src/app/components/message/message.component.ts
+++ src/app/components/message/message.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Location } from '@angular/common';
+import { DatePipe, Location } from '@angular/common';
 
 import { MessageService } from '../../services/message.service';
 import { Message } from '../../classes/message';
