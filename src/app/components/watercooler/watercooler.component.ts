99c85fad8312a7725aabf62c4de39f29768e6b3f
Phillip Stafford <phillip@philliprstafford.com>
2018-03-08 16:22:58 -0500

    Change: Consolidate watercoolere-input and watercool-wrapper and current component.

--- src/app/components/watercooler/watercooler.component.ts
+++ src/app/components/watercooler/watercooler.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
@@ -7,6 +7,9 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 import { Company } from '../../classes/company';
+import { Message } from '../../classes/message';
+import { MessageService } from '../../services/message.service';
+
 import { Watercooler } from '../../classes/watercooler';
 import { WatercoolerService } from '../../services/watercooler.service';
 import { CompanyService } from '../../services/company.service';
@@ -18,17 +21,25 @@ import { CompanyService } from '../../services/company.service';
   styleUrls: ['./watercooler.component.css']
 })
 export class WatercoolerComponent implements OnInit {
+  @Input() message: Message;
 
+
+  private messageText: string;
+
+  messages: Message[] = [];
   watercooler: Watercooler;
   company: Company;
+  date: Date;
+  nextIndex = 0;
+
 
   constructor(private watercoolerService: WatercoolerService,
     private companyService: CompanyService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location,
+    private messageService: MessageService) { }
 
   ngOnInit() {
-    // this.getWatercooler();
     this.getWatercooler().subscribe(_ => {
       ;
       console.log(this.watercooler.company_id);
@@ -59,4 +70,19 @@ export class WatercoolerComponent implements OnInit {
       .subscribe(() => this.goBack());
   }
 
+  addMessage(content: string): void {
+    this.date = new Date(Date.now());
+    console.log(content);
+    content = content.trim();
+    if (!content) { return; }
+    this.messageService.addMessage( {
+    author: "Jane",
+    created: this.date,
+    content: content,
+    watercooler_id: 0
+  }  as Message)
+      .subscribe(message => {
+        this.messages.push(message)
+    });
+  }
 }
