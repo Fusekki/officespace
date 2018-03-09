0231b5b5595759bb78680e237483d6806cc49832
Phillip Stafford <phillip@philliprstafford.com>
2018-03-08 20:15:32 -0500

    Remove log message.

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -74,20 +74,8 @@ export class MessageService {
       );
     }
 
-    //////// Save methods //////////
-    // /** POST: add a new hero to the server */
-    // addHero (hero: Hero): Observable<Hero> {
-    //   return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
-    //     tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
-    //     catchError(this.handleError<Hero>('addHero'))
-    //   );
-    // }
-
-
-
     /** POST: add a new message to the server */
     addMessage (message: Message): Observable<Message> {
-      console.log(message);
       return this.http.post<Message>(this.messagesUrl, message, httpOptions).pipe(
         tap((message: Message) => this.log(`added message w/ id=${message.id}`)),
         catchError(this.handleError<Message>('addMessage'))
