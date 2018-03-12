417424024efa0f582ead613a005d3595afa1de00
Phillip Stafford <phillip@philliprstafford.com>
2018-03-12 17:23:57 -0400

    Add Module.

--- src/app/components/message-board/message-board.component.ts
+++ src/app/components/message-board/message-board.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MatSelectModule} from '@angular/material/select';
+
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
