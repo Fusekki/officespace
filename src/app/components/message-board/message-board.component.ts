8cae0a75a612453ff2b056c107b5269424047be0
Phillip Stafford <phillip@philliprstafford.com>
2018-03-12 20:10:01 -0400

    Add module import.

--- src/app/components/message-board/message-board.component.ts
+++ src/app/components/message-board/message-board.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
