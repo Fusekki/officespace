3248c32b350c1a3cfbfa17f1d3ea6bc4e98a2981
Phillip Stafford <phillip@philliprstafford.com>
2018-03-18 18:09:02 -0400

    fix: Fix properties of class.

--- src/app/classes/mb-post.ts
+++ src/app/classes/mb-post.ts
@@ -5,5 +5,15 @@ export class MbPost {
   created: Date;
   title: string;
   category: number;
-  body: string;
+  content: string;
+
+  constructor(id: number, m_id: number, author: number, created: Date, title: string, category: number, content: string) {
+    this.id = id;
+    this.messageboard_id = m_id;
+    this.author = author;
+    this.created = created;
+    this.title = title,
+    this.category = category,
+    this.content = content;
+  }
 }
