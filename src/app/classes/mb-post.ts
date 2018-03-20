2fcf4ffd588da78ca6d29a4277482e103825c537
Phillip Stafford <phillip@philliprstafford.com>
2018-03-20 18:58:51 -0400

    feat: Add draft property to object

--- src/app/classes/mb-post.ts
+++ src/app/classes/mb-post.ts
@@ -1,15 +1,17 @@
 export class MbPost {
   id: number;
   messageboard_id: number;
+  draft: boolean;
   author: number;
   created: Date;
   title: string;
   category: number;
   content: string;
 
-  constructor(id: number, m_id: number, author: number, created: Date, title: string, category: number, content: string) {
+  constructor(id: number, m_id: number, draft: boolean, author: number, created: Date, title: string, category: number, content: string) {
     this.id = id;
     this.messageboard_id = m_id;
+    this.draft = draft;
     this.author = author;
     this.created = created;
     this.title = title,
