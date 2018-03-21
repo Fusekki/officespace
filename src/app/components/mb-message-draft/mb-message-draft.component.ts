601475f18beb690057838cca2942b860a4b92232
Phillip Stafford <phillip@philliprstafford.com>
2018-03-21 17:10:00 -0400

    feat: Add starter code for component

--- src/app/components/mb-message-draft/mb-message-draft.component.ts
+++ src/app/components/mb-message-draft/mb-message-draft.component.ts
@@ -1,5 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatListModule } from '@angular/material/list';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { MessageBoard } from '../../classes/message-board';
+import { Company } from '../../classes/company';
+import { CompanyService } from '../../services/company.service';
+import { MessageBoardService } from '../../services/message-board.service';
+
+import { MbPost } from '../../classes/mb-post';
+import { MbPostService } from '../../services/mb-post.service';
+
+// Keep until we move to a backend
+import { User } from '../../classes/user';
+import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-mb-message-draft',
   templateUrl: './mb-message-draft.component.html',
