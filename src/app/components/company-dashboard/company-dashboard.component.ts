e6f3c03927099111a46402c44fb75ec57de753fb
Phillip Stafford <phillip@philliprstafford.com>
2018-03-22 11:44:45 -0400

    feat: Add getUser method
    Additionally, import USerService and User class.

--- src/app/components/company-dashboard/company-dashboard.component.ts
+++ src/app/components/company-dashboard/company-dashboard.component.ts
@@ -6,6 +6,9 @@ import { Location } from '@angular/common';
 import { Company } from '../../classes/company';
 import { CompanyService } from '../../services/company.service';
 
+import { User } from '../../classes/user';
+import { UserService } from '../../services/user.service';
+
 
 @Component({
   selector: 'app-company-dashboard',
@@ -14,20 +17,29 @@ import { CompanyService } from '../../services/company.service';
 })
 export class CompanyDashboardComponent implements OnInit {
 
+  user: User;
   company: Company;
 
   constructor(private companyService: CompanyService,
               private route: ActivatedRoute,
-              private location: Location) { }
+              private location: Location,
+              private userService: UserService) { }
 
   ngOnInit() {
     this.getCompany();
+    this.getUser();
   }
 
   getCompany(): void {
     const co = +this.route.snapshot.paramMap.get('co');
     this.companyService.getCompany(co)
       .subscribe(company => this.company = company);
+  }
+
+  getUser(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.userService.getUser(id)
+      .subscribe(user => this.user = user);
   }
 
   goBack(): void {
