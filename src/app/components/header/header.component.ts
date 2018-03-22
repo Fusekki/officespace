74c01683b9f5113a8cd40e3f906467fb6378439a
Phillip Stafford <phillip@philliprstafford.com>
2018-03-22 12:36:27 -0400

    feat: Add user, userService, Activated Route and Location to component.
    Additionally, add the getUser method.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { User } from '../../classes/user';
+import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +13,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  constructor(private userService: UserService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getUser();
+  }
+
+
+  getUser(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.userService.getUser(id)
+      .subscribe(user => this.user = user);
   }
 
 }
--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+// Keep until we move to a backend
+import { User } from '../../classes/user';
+import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-navi',
   templateUrl: './navi.component.html',
   styleUrls: ['./navi.component.css']
 })
 export class NaviComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private userService: UserService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getUser();
+  }
+
+  getUser(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.userService.getUser(id)
+      .subscribe(user => this.user = user);
   }
 
 }
