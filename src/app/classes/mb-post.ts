3eba415395604b02c913f530b8961121a3104025
Phillip Stafford <phillip@philliprstafford.com>
2018-03-26 12:09:27 -0400

    fix: Revert file to previous commit

--- src/app/classes/mb-post.ts
+++ src/app/classes/mb-post.ts
@@ -1,31 +1,21 @@
 export class MbPost {
   id: number;
-  messageboardId: number;
+  messageboard_id: number;
   draft: boolean;
   author: number;
-  created: number;
+  created: Date;
   title: string;
   category: number;
   content: string;
-  state: string;
 
-  constructor(id: number,
-    messageboardId: number,
-    draft: boolean,
-    author: number,
-    created: number,
-    title: string,
-    category: number,
-    content: string,
-    state = 'inactive') {
-      // this.id = id; this.messageboardId = messageboardId;
-      // this.draft = draft; this.author = author; this.created = created;
-      // this.title = title, this.category = category, this.content = content,
-      // this.state = state;
-  }
-
-    toggleState() {
-      console.log('toggleState.');
-      this.state = this.state === 'active' ? 'inactive' : 'active';
+  constructor(id: number, m_id: number, draft: boolean, author: number, created: Date, title: string, category: number, content: string) {
+    this.id = id;
+    this.messageboard_id = m_id;
+    this.draft = draft;
+    this.author = author;
+    this.created = created;
+    this.title = title,
+    this.category = category,
+    this.content = content;
   }
 }
