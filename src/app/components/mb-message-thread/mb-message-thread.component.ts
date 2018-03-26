fc47adf8f6c69f9402911cd41b6762aa4dfae54d
Phillip Stafford <phillip@philliprstafford.com>
2018-03-26 14:13:42 -0400

    fix: Remove duplicate authorName declaration

--- src/app/components/mb-message-thread/mb-message-thread.component.ts
+++ src/app/components/mb-message-thread/mb-message-thread.component.ts
@@ -41,8 +41,6 @@ export class MbMessageThreadComponent implements OnInit {
   date: Date;
   category: number;
   mbpost: MbPost;
-  authorName: string;
-
 
   constructor(private projectService: ProjectService,
               private route: ActivatedRoute,
