86c6922eed572cafd53a1b91d131bc51adf7ae0c
Phillip Stafford <phillip@philliprstafford.com>
2018-03-27 08:47:58 -0400

    feat: Add subscribers property to MbPost class

--- src/app/classes/mb-post.ts
+++ src/app/classes/mb-post.ts
@@ -7,15 +7,5 @@ export class MbPost {
   title: string;
   category: number;
   content: string;
-
-  constructor(id: number, m_id: number, draft: boolean, author: number, created: any, title: string, category: number, content: string) {
-    this.id = id;
-    this.messageboardId = m_id;
-    this.draft = draft;
-    this.author = author;
-    this.created = created;
-    this.title = title,
-    this.category = category,
-    this.content = content;
-  }
+  subscribers: number[];
 }
