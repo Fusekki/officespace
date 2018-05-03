400159552343316db5da53bf15458e5e3c950f52
Phillip Stafford <phillip@philliprstafford.com>
2018-05-03 11:40:36 -0400

    feat: Add beginning code

--- src/app/components/to-dos/to-dos.component.ts
+++ src/app/components/to-dos/to-dos.component.ts
@@ -1,5 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatListModule} from '@angular/material/list';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Project } from '../../classes/project';
+import { ProjectService } from '../../services/project.service';
+
+import { ToDo } from '../../classes/to-do';
+
+// Keep until we move to a backend
+import { User } from '../../classes/user';
+import { UserService } from '../../services/user.service';
+
 @Component({
   selector: 'app-to-dos',
   templateUrl: './to-dos.component.html',
@@ -7,9 +23,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToDosComponent implements OnInit {
 
-  constructor() { }
+  selectedCategory: string;
+  project: Project;
+  user: User;
+  users: User[];
+
+  constructor(private projectService: ProjectService,
+              private route: ActivatedRoute,
+              private location: Location,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.getProject();
+    this.getUsers();
+    this.getUser();
   }
+
+
+  getProject(): void {
+    const pr = +this.route.snapshot.paramMap.get('pr');
+    this.projectService.getProject(pr)
+      .subscribe(project => this.project = project);
+  }
+
+  getUser(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.userService.getUser(id)
+      .subscribe(user => this.user = user);
+  }
+
+
+
+  getUsers(): void {
+    this.userService.getUsers()
+      .subscribe(users => this.users = users);
+  }
+
+
+  goBack(): void {
+    this.location.back();
+  }
+
 
 }
