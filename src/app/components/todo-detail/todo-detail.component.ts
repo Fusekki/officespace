6610464c2223f59ee9af6fab0dee20294e60a250
Phillip Stafford <phillip@philliprstafford.com>
2018-05-08 14:30:36 -0400

    feat: Add starter code

--- src/app/components/todo-detail/todo-detail.component.ts
+++ src/app/components/todo-detail/todo-detail.component.ts
@@ -1,4 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
+
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatListModule} from '@angular/material/list';
+
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+import { Project } from '../../classes/project';
+import { ProjectService } from '../../services/project.service';
+
+import { ToDo } from '../../classes/to-do';
+import { ToDoService } from '../../services/todo.service';
+// Keep until we move to a backend
+import { User } from '../../classes/user';
+import { UserService } from '../../services/user.service';
+
 
 @Component({
   selector: 'app-todo-detail',
@@ -7,9 +28,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoDetailComponent implements OnInit {
 
-  constructor() { }
+authorName: string;
+project: Project;
+user: User;
+todos: ToDo[] = [];
+users: User[] = [];
+date: Date;
+todo: ToDo;
 
-  ngOnInit() {
-  }
+constructor(private projectService: ProjectService,
+            private route: ActivatedRoute,
+            private location: Location,
+            private userService: UserService,
+            private todoService: ToDoService,
+            private router: Router) { }
+
+ngOnInit() {
+  const to = +this.route.snapshot.paramMap.get('to');
+  this.getProject();
+  this.getTodos().subscribe( todos => {
+    this.todo = this.todos.find(todo => todo.id == to);
+  });
+  this.getUser();
+  this.getUsers().subscribe(_ => {
+    ;
+    // this.authorName = this.users.find(user => user.id == this.mbpost.author).fullName;
+  });
+}
+
+
+getProject(): void {
+  const co = +this.route.snapshot.paramMap.get('co');
+  this.projectService.getProject(co)
+    .subscribe(project => this.project = project);
+}
+
+
+getUsers() {
+  return this.userService.getUsers()
+    .map(users => this.users = users);
+}
+
+
+goBack(): void {
+  this.location.back();
+}
+
+
+getTodo(): void {
+  const to = +this.route.snapshot.paramMap.get('to');
+  this.todoService.getToDo(to)
+    .subscribe(todo => this.todo = todo);
+}
+
+
+
+getTodos() {
+  return this.todoService.getToDos()
+    .map(todos => this.todos = todos);
+}
+
+getUser(): void {
+  const id = +this.route.snapshot.paramMap.get('id');
+  this.userService.getUser(id)
+    .subscribe(user => this.user = user);
+}
 
 }
